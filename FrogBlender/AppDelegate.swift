0916c2e570858d046632171847cb35c5e91d8b8e
Horváth Balázs <balazs630@icloud.com>
2017-07-01 13:10:00 +0200

    Add initial Xcode Cocoa project

---
+++ FrogBlender/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FrogBlender
+//
+//  Created by Horváth Balázs on 2017. 07. 01..
+//  Copyright © 2017. Horváth Balázs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
