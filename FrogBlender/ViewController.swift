b7df31c7a96645f2c1b03e9f1e064bb301a3d241
Horváth Balázs <balazs630@icloud.com>
2017-07-01 15:21:05 +0200

    Add full width video player to the window, add simple video playback functionaloty

--- FrogBlender/ViewController.swift
+++ FrogBlender/ViewController.swift
@@ -7,21 +7,44 @@
 //
 
 import Cocoa
+import AVKit
+import AVFoundation
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var playerView: AVPlayerView!
+
+    var player = AVPlayer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+
+        //Loop video playback
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: self.player.currentItem, queue: nil, using: { (_) in
+            DispatchQueue.main.async {
+                self.player.seek(to: kCMTimeZero)
+                self.player.play()
+            }
+        })
+
+        playVideo(fileNamed: "speed-1", type: "mp4")
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    private func playVideo(fileNamed: String, type: String) {
+        guard let path = Bundle.main.path(forResource: fileNamed, ofType: type) else {
+            debugPrint("\(fileNamed).\(type) not found")
+            return
         }
+
+        player = AVPlayer(url: URL(fileURLWithPath: path))
+        playerView!.player = player
+        playerView!.player?.play()
     }
 
+    override func viewDidAppear() {
+        // Disable green zoom button
+        view.window?.standardWindowButton(NSWindowButton.zoomButton)?.isEnabled = false
+    }
 
 }
-
